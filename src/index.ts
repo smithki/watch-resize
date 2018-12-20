e90d0b2c267009697471d1517ae6942ed1d963fc
Ian K Smith <smithki707@gmail.com>
2018-12-19 17:52:12 -0700

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,25 @@ import { throttleTime } from 'rxjs/operators';
 
 // --- Business logic ------------------------------------------------------- //
 
+/**
+ * Returns a Promise that resolves to a RxJS Observable firing when the given
+ * element changes in size.
+ *
+ * @param element HTMLElement to watch for size changes.
+ */
 export function watchResize<T extends HTMLElement>(
   element: T,
 ): Promise<Observable<Event>> {
   return new Promise((resolve, reject) => {
-    if (!element) reject('Element must be defined');
+    if (!element) reject('The given `HTMLElement` must be defined.');
 
+    // Ensure we are relatively positioned so that nested <object> is correctly
+    // positioned.
     if (getComputedStyle(element).position === 'static') {
       element.style.position = 'relative';
     }
 
+    // Create a nested browsing context using <object>
     const obj = document.createElement('object');
     obj.setAttribute(
       'style',
@@ -22,6 +31,8 @@ export function watchResize<T extends HTMLElement>(
     );
     obj.type = 'text/html';
     obj.data = 'about:blank';
+
+    // When the <object> loads, resolve the Observable.
     obj.onload = () => {
       if (obj.contentDocument && obj.contentDocument.defaultView) {
         const viewContext = obj.contentDocument.defaultView;
@@ -31,10 +42,13 @@ export function watchResize<T extends HTMLElement>(
           ),
         );
       } else {
-        reject('Could not build `HTMLObjectElement` to watch element resize.');
+        reject(
+          'Could not build a nested browsing context to watch for DOM element size changes.',
+        );
       }
     };
 
+    // Append the <object> to the given element.
     element.appendChild(obj);
   });
 }
