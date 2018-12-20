1ef47b8a86ab1c2f31c81489090505735565c86e
Ian K Smith <smithki707@gmail.com>
2018-12-20 09:02:20 -0700

    More descriptive comments

--- src/lib.ts
+++ src/lib.ts
@@ -6,8 +6,8 @@ import { throttleTime } from 'rxjs/operators';
 // --- Business logic ------------------------------------------------------- //
 
 /**
- * Returns a Promise that resolves to a RxJS Observable firing when the given
- * element changes in size.
+ * Returns a Promise that resolves to a RxJS Observable. The Observable fires
+ * changes in size of the given DOM element are detected.
  *
  * @param element HTMLElement to watch for size changes.
  */
@@ -17,13 +17,13 @@ export function watchResize<T extends HTMLElement>(
   return new Promise((resolve, reject) => {
     if (!element) reject('The given `HTMLElement` must be defined.');
 
-    // Ensure we are relatively positioned so that nested <object> is correctly
-    // positioned.
+    // Ensure we are relatively positioned so that the nested browsing context
+    // is correctly sized and positioned.
     if (getComputedStyle(element).position === 'static') {
       element.style.position = 'relative';
     }
 
-    // Create a nested browsing context using <object>
+    // Create a nested browsing context using an <object> element.
     const obj = document.createElement('object');
     obj.setAttribute(
       'style',
@@ -36,6 +36,8 @@ export function watchResize<T extends HTMLElement>(
     obj.onload = () => {
       if (obj.contentDocument && obj.contentDocument.defaultView) {
         const viewContext = obj.contentDocument.defaultView;
+        // We create the Observable from the `resize` event attached to the
+        // nested browsing context.
         resolve(
           fromEvent(viewContext, 'resize').pipe(
             throttleTime(0, animationFrameScheduler),
@@ -48,7 +50,7 @@ export function watchResize<T extends HTMLElement>(
       }
     };
 
-    // Append the <object> to the given element.
+    // Append the <object> to the target element.
     element.appendChild(obj);
   });
 }
